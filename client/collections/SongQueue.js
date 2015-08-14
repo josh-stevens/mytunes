7dc3badc1f2627eeb6d44566552120a5d980c69f
josh-stevens <jcstevens12@outlook.com>
2015-08-14 15:23:38 -0500

    passed LEV, APP,

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,15 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+  this.on('add', function() {
+    this.length === 1 && this.playFirst();
+  })
+    //this.playFirst();
+  },
+
+  playFirst: function(){
+    this.at(0).play();
   }
+
 
 });
